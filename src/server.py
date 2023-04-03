6bbae73e6d9e6599cb8191701c387e70371f041b
Nubaz <lucapaulf@gmail.com>
2023-04-03 11:43:37 +0300

    Sistem de logare rudimentar

--- src/server.py
+++ src/server.py
@@ -6,7 +6,7 @@ import os, enum
 
 load_dotenv(find_dotenv())
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 app.secret_key = os.getenv('APP_SECRET')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
@@ -27,13 +27,33 @@ class User(db.Model):
     parola = db.Column(db.String(255), nullable=False)
     rol = db.Column(db.Enum(Roles))
 
-    def __repr__(self):
-        return f"<User id={self.id} nume={self.nume} prenume={self.prenume} username={self.username} email={self.email} parola={self.parola} rol={self.rol}>"
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    error_msg = ''
 
-with app.app_context():
-    print(User.query.all())
+    if request.method == 'POST' and 'username' in request.form and 'parola' in request.form:
+        username = request.form['username']
+        parola = request.form['parola']
 
+        with app.app_context():
+            user = User.query.filter_by(username=username, parola=parola).all()
 
-@app.route('/')
-def index(name=None):
-    return render_template('index.html', name=name)
+        if user:
+            user = [u.__dict__ for u in user][0]
+            session['loggedIn'] = True
+            session['id'] = user['id']
+            session['username'] = user['username']
+
+            return 'Logare cu succes!'
+        else:
+            error_msg = 'Date de logare incorecte!'
+
+    return render_template('login.html', error_msg=error_msg)
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedIn', None)
+    session.pop('id', None)
+    session.pop('username', None)
+
+    return redirect(url_for('login'))
